55ea15bd33fa2bd2c8a5b086323dfbbcae4d9e30
Jon Moss <me@jonathanmoss.me>
2018-06-16 08:46:53 -0400

    Add FreeBSD support

--- jenkins/scripts/node-test-commit.sh
+++ jenkins/scripts/node-test-commit.sh
@@ -15,6 +15,20 @@ echo FLAKY_TESTS_MODE=$FLAKY_TESTS_MODE
 
 . ./build/jenkins/scripts/select-compiler.sh
 
-NODE_TEST_DIR=${HOME}/node-tmp NODE_COMMON_PORT=15000 PYTHON=python FLAKY_TESTS=$FLAKY_TESTS_MODE make run-ci -j $(getconf _NPROCESSORS_ONLN)
+if getconf _NPROCESSORS_ONLN
+then
+  MAKE_JOB_COUNT=$(getconf _NPROCESSORS_ONLN)
+else
+  MAKE_JOB_COUNT=$(getconf NPROCESSORS_ONLN)
+fi
+
+if test $NODE_NAME = *"freebsd"*
+then
+  MAKE=gmake
+else
+  MAKE=make
+fi
+
+NODE_TEST_DIR=${HOME}/node-tmp NODE_COMMON_PORT=15000 PYTHON=python FLAKY_TESTS=$FLAKY_TESTS_MODE $MAKE run-ci -j $MAKE_JOB_COUNT
 
 . ./build/jenkins/scripts/node-test-commit-diagnostics.sh after
