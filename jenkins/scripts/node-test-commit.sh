d4009c0ea0a48ccfbc535461db8ed0145bae1497
Jon Moss <me@jonathanmoss.me>
2018-06-18 23:06:20 -0400

    start linux

--- jenkins/scripts/node-test-commit.sh
+++ jenkins/scripts/node-test-commit.sh
@@ -22,6 +22,12 @@ else
   MAKE_JOB_COUNT=$(getconf NPROCESSORS_ONLN)
 fi
 
+MAKE_ARGS="-j $MAKE_JOB_COUNT"
+
+if [ $(make -v | grep 'GNU Make 4' -c) -ne 0 ]; then
+  MAKE_ARGS="$MAKE_ARGS --output-sync=target"
+fi
+
 if [[ $NODE_NAME = *"freebsd"* ]]; then
   MAKE=gmake
 else
@@ -34,6 +40,12 @@ else
   CONFIG_FLAGS=""
 fi
 
-NODE_TEST_DIR=${HOME}/node-tmp NODE_COMMON_PORT=15000 PYTHON=python FLAKY_TESTS=$FLAKY_TESTS_MODE CONFIG_FLAGS=$CONFIG_FLAGS $MAKE run-ci -j $MAKE_JOB_COUNT
+if test $nodes = "centos6-64-gcc48"; then
+  . /opt/rh/devtoolset-2/enable
+elif [[ "$nodes" =~ centos[67]-(arm)?64-gcc6 ]]; then
+  . /opt/rh/devtoolset-6/enable
+fi
+
+NODE_TEST_DIR=${HOME}/node-tmp NODE_COMMON_PORT=15000 PYTHON=python FLAKY_TESTS=$FLAKY_TESTS_MODE CONFIG_FLAGS=$CONFIG_FLAGS V=1 $MAKE run-ci $MAKE_ARGS
 
 . ./build/jenkins/scripts/node-test-commit-diagnostics.sh after
