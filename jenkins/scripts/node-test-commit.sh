eb98afbdfc528c75da85081bf72becaa4910ff43
Jon Moss <me@jonathanmoss.me>
2018-06-19 14:35:22 -0400

    try and migrate to node-test-commit

--- jenkins/scripts/node-test-commit.sh
+++ jenkins/scripts/node-test-commit.sh
@@ -12,6 +12,16 @@ else
 fi
 echo FLAKY_TESTS_MODE=$FLAKY_TESTS_MODE
 
+if [[ $NODE_NAME = *"aix61"* ]]; then
+  # LIBPATH must be set to /opt/freeware/lib for the git plugin to work, however
+  # it must be unset for the build.  The ansible start script sets the libpath
+  # so that when git runs its set, but we must unset it here otherwise it
+  # causes the build to find the 32 bit stdc++ library instead of the 64 bit one
+  # that we need when running binaries (like mksnapshot) since our target is 64 bit
+  unset LIBPATH
+  echo LIBPATH:$LIBPATH
+fi
+
 . ./build/jenkins/scripts/select-compiler.sh
 
 if [[ $NODE_NAME = *"smartos"* ]]; then
@@ -20,6 +30,8 @@ elif [[ $NODE_NAME = *"centos7-arm64"* ]] ||
   [[ $NODE_NAME = *"docker-armv7"* ]] ||
   [[ $NODE_NAME = *"ubuntu1604-arm64"* ]]; then
   MAKE_JOB_COUNT=2
+elif [[ $NODE_NAME = *"aix61"* ]]; then
+  MAKE_JOB_COUNT=5
 elif getconf _NPROCESSORS_ONLN; then
   MAKE_JOB_COUNT=$(getconf _NPROCESSORS_ONLN)
 else
@@ -36,13 +48,15 @@ if [ $(make -v | grep 'GNU Make 4' -c) -ne 0 ]; then
   MAKE_ARGS="$MAKE_ARGS --output-sync=target"
 fi
 
-if [[ $NODE_NAME = *"freebsd"* ]]; then
+if [[ $NODE_NAME = *"freebsd"* ]] ||
+  [[ $NODE_NAME = *"aix61-ppc"* ]]; then
   MAKE=gmake
 else
   MAKE=make
 fi
 
-if [[ $NODE_NAME = *"ubuntu1404-ppc"* ]]; then
+if [[ $NODE_NAME = *"ubuntu1404-ppc"* ]] ||
+  [[ $NODE_NAME = *"aix61-ppc"* ]]; then
   CONFIG_FLAGS="--dest-cpu=ppc64"
 else
   CONFIG_FLAGS=""
