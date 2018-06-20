54ef04843ee9edeb91a564a74d3bef40e1223da9
Jon Moss <me@jonathanmoss.me>
2018-06-20 15:32:14 -0400

    fixup aix

--- jenkins/scripts/node-test-commit.sh
+++ jenkins/scripts/node-test-commit.sh
@@ -5,22 +5,17 @@ git clone https://github.com/nodejs/build.git
 
 . ./build/jenkins/scripts/node-test-commit-pre.sh
 
+if [[ $NODE_NAME = *"aix61"* ]]; then
+  curl https://raw.githubusercontent.com/maclover7/build/edfc68bc6e389dd90d6e116781716d3ca41d178c/jenkins/scripts/node-test-commit-aix.sh | bash -ex -s
+  exit 0
+fi
+
 if [[ $IGNORE_FLAKY_TESTS = "true" ]]; then
   FLAKY_TESTS_MODE=dontcare
 else
   FLAKY_TESTS_MODE=run
 fi
 echo FLAKY_TESTS_MODE=$FLAKY_TESTS_MODE
-
-if [[ $NODE_NAME = *"aix61"* ]]; then
-  # LIBPATH must be set to /opt/freeware/lib for the git plugin to work, however
-  # it must be unset for the build.  The ansible start script sets the libpath
-  # so that when git runs its set, but we must unset it here otherwise it
-  # causes the build to find the 32 bit stdc++ library instead of the 64 bit one
-  # that we need when running binaries (like mksnapshot) since our target is 64 bit
-  unset LIBPATH
-  echo LIBPATH:$LIBPATH
-fi
 
 . ./build/jenkins/scripts/select-compiler.sh
 
@@ -30,8 +25,6 @@ elif [[ $NODE_NAME = *"centos7-arm64"* ]] ||
   [[ $NODE_NAME = *"docker-armv7"* ]] ||
   [[ $NODE_NAME = *"ubuntu1604-arm64"* ]]; then
   MAKE_JOB_COUNT=2
-elif [[ $NODE_NAME = *"aix61"* ]]; then
-  MAKE_JOB_COUNT=5
 elif getconf _NPROCESSORS_ONLN; then
   MAKE_JOB_COUNT=$(getconf _NPROCESSORS_ONLN)
 else
@@ -44,19 +37,17 @@ fi
 
 MAKE_ARGS="-j $MAKE_JOB_COUNT"
 
-if [[ $NODE_NAME != *"aix61"* ]] && [ $(make -v | grep 'GNU Make 4' -c) -ne 0 ]; then
+if [ $(make -v | grep 'GNU Make 4' -c) -ne 0 ]; then
   MAKE_ARGS="$MAKE_ARGS --output-sync=target"
 fi
 
-if [[ $NODE_NAME = *"freebsd"* ]] ||
-  [[ $NODE_NAME = *"aix61-ppc"* ]]; then
+if [[ $NODE_NAME = *"freebsd"* ]]; then
   MAKE=gmake
 else
   MAKE=make
 fi
 
-if [[ $NODE_NAME = *"ubuntu1404-ppc"* ]] ||
-  [[ $NODE_NAME = *"aix61-ppc"* ]]; then
+if [[ $NODE_NAME = *"ubuntu1404-ppc"* ]]; then
   CONFIG_FLAGS="--dest-cpu=ppc64"
 else
   CONFIG_FLAGS=""
@@ -77,16 +68,16 @@ if [[ "$NODE_LABELS" =~ docker-armv7 ]]; then
     *) echo Error: Unsupported label $nodes; exit 1
   esac
 
-  echo " \
+  exec_cmd=" \
     NODE_TEST_DIR=${HOME}/node-tmp NODE_COMMON_PORT=15000 PYTHON=python \
     FLAKY_TESTS=$FLAKY_TESTS_MODE CONFIG_FLAGS=$CONFIG_FLAGS \
     $MAKE run-ci $MAKE_ARGS \
-  " > node-ci-exec
+  "
+
+  echo $exec_cmd > node-ci-exec
   sudo docker-node-exec.sh -v $debian
 else
-  NODE_TEST_DIR=${HOME}/node-tmp NODE_COMMON_PORT=15000 PYTHON=python \
-  FLAKY_TESTS=$FLAKY_TESTS_MODE CONFIG_FLAGS=$CONFIG_FLAGS \
-  $MAKE run-ci $MAKE_ARGS
+  sh -c $exec_cmd
 fi
 
 . ./build/jenkins/scripts/node-test-commit-diagnostics.sh after
